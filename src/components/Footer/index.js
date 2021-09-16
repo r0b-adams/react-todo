5c1841acccef760fb88b5529e44ae4539e9ca3b7
Robert Adams <adamsiii.robert@gmail.com>
2021-09-16 16:47:11 -0700

    created Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <nav>
+                <a href='https://github.com/comatosino'>
+                    <img src='https://github.githubassets.com/apple-touch-icon-114x114.png'
+                         height='50px'
+                         alt='GitHub Logo'>
+                    </img>
+                </a>
+            </nav>
+        </footer>
+    )
+}
