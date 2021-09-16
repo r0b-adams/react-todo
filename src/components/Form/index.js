af766b0c51036da8112383614fd32361d6fe2fab
Robert Adams <adamsiii.robert@gmail.com>
2021-09-16 15:06:07 -0700

    populated for component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Form() {
+export default function Form({handleInputChange, handleSubmit, value}) {
     return (
-        <div>
-
-        </div>
+        <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text'
+               placeholder='Enter Todo Item'
+               value={value}
+               onChange={(e) => handleInputChange(e)}>
+        </input>
+        <button type='submit'>Save</button>
+      </form>
     )
 }
