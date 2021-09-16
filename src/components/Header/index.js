30502b3263b644eb9e1e9a567dd62b333b68ac4a
Robert Adams <adamsiii.robert@gmail.com>
2021-09-16 14:07:33 -0700

    created header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                React Todo List
+            </h1>
+        </header>
+    )
+}
